13ecd6554f41214138a96677cf00f46396f47861
moranyosef93 <moranyosef93@gmail.com>
2017-07-06 23:11:23 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+   
+
+    new Vivus("mand", {
+        type: 'delayed',
+        duration: 100,
+        dashGap: 150,
+        selfDestroy:true,
+        file: 'backgrounds/OBZZVD1.svg'
+    }
+
+            <object id="mand" type="image/svg+xml" data="backgrounds/OBZZVD1.svg"></object>
+
+    })
